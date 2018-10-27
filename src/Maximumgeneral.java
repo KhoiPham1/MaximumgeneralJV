9b10eb1f07861d334c5a06bdbffddf84f691ea90
Khoi Pham <khoi171195@gmail.com>
2018-10-27 16:44:02 +0700

    firstcommit

---
+++ src/Maximumgeneral.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Maximumgeneral {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = input.nextInt();
+        a = Math.abs(a);
+        System.out.println("Enter b: ");
+        b = input.nextInt();
+        b = Math.abs(b);
+        if (a==0 || b ==0){
+            System.out.println("No greatest common factor");
+        }
+        while (a!=b){
+            if (a>b){
+                a = a -b;
+            }else {
+                b = b-a;
+            }
+        }
+        System.out.println("Greatest common factor: " + a);
+
+    }
+}
